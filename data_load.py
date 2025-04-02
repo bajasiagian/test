0f561cc39adf061d477c16a1dfe8b1a15da740ae
Baja Stephanus <146203885+bajasiagian@users.noreply.github.com>
2025-04-02 23:00:07 +0700

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -1,5 +1,5 @@
 import gspread
-from google.oauth2.service_account import Credentials
+from google.oauth2.service_account import ServiceAccountCredentials
 import pandas as pd
 import requests
 import json
